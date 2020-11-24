5342b4fcae9f617ffb152c7244413c88e4ca08b7
Emil Backlund <emilbacklund@outlook.com>
2020-11-24 13:05:29 +0100

    Jumped back to working function

--- 777.php
+++ 777.php
@@ -8,15 +8,20 @@
 
 
 $object_check_string = new Is_seven;
-// $object_check_string->set_string_to_check($_POST("user_entered_string"));
 
-add_action('woocommerce_before_cart', array($object_check_string, 'check_string'));
+
+do_action('woocommerce_before_shop_item', array($object_check_string, 'clean_string_and_obj'));
 
 class Is_seven
 {
     public $string_to_check;
     public $clean_string;
-   
+
+    public function __contruct()
+    {
+        self::clean_string_and_obj();
+    }
+
     public function set_string_to_check($string_to_check)
     {
         $this->string_to_check = $string_to_check;
@@ -25,17 +30,23 @@ class Is_seven
     {
         return $this->string_to_check;
     }
-    public function check_string()
+    public function clean_string_and_obj()
     {
-        echo $string_to_check;
-    }
-}
-?>
+        $clean_string->sanitize_text_field($_POST["user_entered_string"]);
+        $this->set_string_to_check($clean_string);
 
-<form action="" method="post">
+        if (! $this->get_string_to_check()) {?>
+<form
+  action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"
+  method="post">
   <input type="text" name="user_entered_string" id="" placeholder="Enter text">
   <input type="submit">
 </form>
-
-
-
+<?php
+           } elseif (strlen($this->get_string_to_check()) === 7) {
+               echo 'String is exactly 7 characters long';
+           } else {
+               echo 'string is <b>NOT</b> exactly 7 characters long';
+           }
+    }
+}
