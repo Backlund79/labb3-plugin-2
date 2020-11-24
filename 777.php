6b3ea7c18d4cfde95c150bcf22980caab8b29356
Emil Backlund <emilbacklund@outlook.com>
2020-11-24 11:20:52 +0100

    Progress update

--- 777.php
+++ 777.php
@@ -4,9 +4,14 @@
 * Author: Emil
 */
 
-require 'acf-field-data.php';
+// require 'acf-field-data.php';
+
 
 $object_check_string = new Is_seven;
+
+// $sanitized_string = sanitize_text_field($_POST["user_entered_String"]);
+// var_dump($_POST["user_entered_String"]);
+// $object_check_string->set_string_to_check($sanitized_string);
 
 do_action('woocommerce_before_shop_loop_item', array($object_check_string, 'check_string'));
 
@@ -14,18 +19,30 @@ class Is_seven
 {
     public $string_to_check;
 
-    public function check_string($string_to_check)
+    public function __construct()
     {
-        if (strlen($string_to_check) === 7) {
+        self::check_form();
+    }
+
+    public function set_string_to_check($string_to_check)
+    {
+        $this->string_to_check = $string_to_check;
+    }
+    public function get_string_to_check()
+    {
+        return $this->string_to_check;
+    }
+    public function check_form()
+    {
+        if (! $this->string_to_check) {
+            echo '<form action="777.php" method="post">
+      <input type="text" name="user_entered_string" id="" placeholder="Enter text">
+      <input type="submit" value="Submit">
+      </form>';
+        } elseif (strlen($string_to_check) === 7) {
             echo "<p>String is seven characters long</p>";
         } else {
             echo "<p>String is <b>NOT</b> seven characters long</p>";
         }
     }
 }
-
-?>
-<form action="777.php" method="post">
-<input type="text" name="user_entered_string" id="" placeholder="Enter text">
-<input type="submit" value="Submit">
-</form>
