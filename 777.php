02f32cb084be04204d8ef207302d8711bc1e85a2
Emil Backlund <emilbacklund@outlook.com>
2020-11-24 14:39:50 +0100

    Function working need to add security to field

--- 777.php
+++ 777.php
@@ -8,14 +8,12 @@
 
 
 $object_check_string = new Is_seven;
-// $object_check_string->set_string_to_check($_POST("user_entered_string"));
 
 add_action('woocommerce_before_cart', array($object_check_string, 'check_string'));
 
 class Is_seven
 {
-    public $string_to_check;
-    public $clean_string;
+    public $string_to_check = '';
    
     public function set_string_to_check($string_to_check)
     {
@@ -25,15 +23,25 @@ class Is_seven
     {
         return $this->string_to_check;
     }
+   
+
     public function check_string()
     {
-        if (isset($string_to_check)) {
-            echo $string_to_check;
-        } else {?>
+        ?>
 <form action="" method="post">
     <input type="text" name="user_entered_string" id="" placeholder="Enter text">
     <input type="submit">
-</form> <?php
+</form>
+<?php
+if (strlen($this->get_string_to_check()) === 7) {
+            echo 'string is 7 long';
+        } else {
+            if ($this->get_string_to_check()) {
+                echo 'string is not 7 long';
+            }
         }
     }
 }
+
+$obj_str = isset($_POST['user_entered_string']) ? $_POST['user_entered_string'] : '' ;
+$object_check_string->set_string_to_check($obj_str);
